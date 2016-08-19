552993c5997fa8421bc11728948a4b1e4a54ea5f
Cho Chang <cyleung.berkeley@gmail.com>
2016-08-18 20:26:05 -0700

    changed table style

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,106 +1,108 @@
 /* ----- event handlers ----- */
 
 //$('#search').on('keypress blur', function(evt) {
-  //if (evt.keyCode === 13 || evt.type === 'blur') doSearch();
+//if (evt.keyCode === 13 || evt.type === 'blur') doSearch();
 //});
 
 //$('[type="radio"]').on('change', function() { render(); });
 
 // freebies slideToggle effect
 $("#ibooks").click(function() {
-    $("#ibookslist").slideToggle();
-    });
+  $("#ibookslist").slideToggle();
+});
 
 $("#trials").click(function() {
-    $("#trialslist").slideToggle();
-    });
+  $("#trialslist").slideToggle();
+});
 
-    $("#coupons").click(function() {
-      $("#couponslist").slideToggle();
-      });
+$("#coupons").click(function() {
+  $("#couponslist").slideToggle();
+});
 
-$("#submit").click(function () {
-    var location = $("#location").val()
+$("#submit").click(function() {
+  var location = $("#location").val()
     // var radius = $("#radius").val()
-    var radius = 15
-    var firstDate = $("#date1").val()
-    var lastDate = $("#date2").val()
-    var keywords = $("#keywords").val()
+  var radius = 15
+  var firstDate = $("#date1").val()
+  var lastDate = $("#date2").val()
+  var keywords = $("#keywords").val()
 
-    $.post("/api/events", {
-        location: location,
-        radius: radius,
-        firstDate: firstDate,
-        lastDate: lastDate,
-        keywords: keywords,
+  $.post("/api/events", {
+      location: location,
+      radius: radius,
+      firstDate: firstDate,
+      lastDate: lastDate,
+      keywords: keywords,
     })
-   .then(function (res) {
-       if(res.ebres.pagination.object_count == 0) {
-            $('#results').html('<p>No Free Events meeting search criteria</p>')
-       } else {
+    .then(function(res) {
+      if (res.ebres.pagination.object_count == 0) {
+        $('#results').html('<p>No Free Events meeting search criteria</p>')
+      } else {
 
-            var table = ""
-            var label = ""
-            res.ebres.events.forEach( function(e,i) {
-                label = "Save"
-                if (res.user) {
-                    if ( res.user.events.indexOf(`e${ e.id }`) > -1 ) {
-                        label = "Delete"
-                    }
-                }
-                table = table + `
+        var table = ""
+        var label = ""
+        res.ebres.events.forEach(function(e, i) {
+          label = "Save"
+          if (res.user) {
+            if (res.user.events.indexOf(`e${ e.id }`) > -1) {
+              label = "Delete"
+            }
+          }
+          table = table + `
                     <tr>
-                    <td><a href="${ e.url }" target="_blank">${ e.name.text }</a></td>
-                    <td>${ moment.utc(e.start.local).format('MMMM D, YYYY') }</td>
-                    <td>${ moment.utc(e.start.local).format('h:mm a') }</td>
+                    <td id="td-URL"><a href="${ e.url }" target="_blank">${ e.name.text }</a></td>
+                    <td id="td-Date">${ moment.utc(e.start.local).format('MMMM D, YYYY') }</td>
+                    <td id="td-Time">${ moment.utc(e.start.local).format('h:mm a') }</td>
                     <td><button class="event-button" id="e${ e.id }">${label}</button></td>
                     </tr>
                 `
-            })
-            table = `<h2>Free Events Near You</h2><table><tr><th>Event</th><th>Date</th><th>Time</th><th></th>${ table }</table>`
-            $('#results').html(table)
-       }
-   })
+        })
+        table = `<h2 id="search-h2">Free Events Near You</h2><table><tr><th id="th-Event">Event</th><th id="th-Date">Date</th><th id="th-Time">Time</th><th></th>${ table }</table>`
+        $('#results').html(table)
+      }
+    })
 })
 
-$(document).on('click', '.event-button', function (evt) {
-    if ( $(evt.target).text() == "Save" ) {
-        $.post("/users", {
-            etid: evt.target.id,
-        })
-        .then(function (res) {
-            if (res.error) {
-                if (res.failedSave) {
-                    alert("Failed save...")
-                } else {
-                    alert("Logging in...")
-                    window.location = '/auth/google'
-                }
-            } else {
-                $(`#${res.etid}`).text('Delete')
-            }
-        })
-    } else {
-        $.ajax({
-            method: "DELETE",
-            url: "/users",
-            data: { etid: evt.target.id }
-        })
-        // $.ajax.delete("/users", {
-        //     etid: evt.target.id,
-        // })
-        .then(function (res) {
+$(document).on('click', '.event-button', function(evt) {
+  if ($(evt.target).text() == "Save") {
+    $.post("/users", {
+        etid: evt.target.id,
+      })
+      .then(function(res) {
+        if (res.error) {
+          if (res.failedSave) {
+            alert("Failed save...")
+          } else {
+            alert("Logging in...")
+            window.location = '/auth/google'
+          }
+        } else {
+          $(`#${res.etid}`).text('Del')
+        }
+      })
+  } else {
+    $.ajax({
+        method: "DELETE",
+        url: "/users",
+        data: {
+          etid: evt.target.id
+        }
+      })
+      // $.ajax.delete("/users", {
+      //     etid: evt.target.id,
+      // })
+      .then(function(res) {
 
-            if (res.error) {
-                if (res.failedDelete) {
-                    alert("Failed delete...")
-                } else {
-                    alert("Not logged in...")
-                    window.location = '/auth/google'
-                }
-            } else {
-                $(`#${res.etid}`).text('Save')
-            }
-        })
-    }
+        if (res.error) {
+          if (res.failedDelete) {
+            alert("Failed delete...")
+          } else {
+            alert("Not logged in...")
+            window.location = '/auth/google'
+          }
+        } else {
+          $(`#${res.etid}`).text('Save')
+        }
+      })
+  }
 })
